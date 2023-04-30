1fa3522211a47c4eab0f2fe2ab225ac149df5e52
Méndez <47764656+El-Mendez@users.noreply.github.com>
2023-04-29 20:40:17 -0600

    método naive listo

--- test.cpp
+++ test.cpp
@@ -1,9 +1,10 @@
 #include <openssl/evp.h>
 #include <cstring>
-#include <iostream>
+#include <mpi.h>
 #include "crypto.h"
 
-#define KEY 1L
+#define KEY 2L
+#define MAX_KEY (1L << 56)
 #define BUFFER_SIZE 128
 
 unsigned char SECRET_MSG[BUFFER_SIZE] = "abcdefghjiklmnopqrstuvwxyz";
@@ -27,18 +28,56 @@ bool key_matches(long key, unsigned char* cipher_text, int cipher_len, unsigned 
     return contains(buffer, SECRET_SECTION);
 }
 
-int main() {
+int main(int argc, char** argv) {
     init_cipher();
-
     unsigned char cipher_text[BUFFER_SIZE];
     unsigned char buffer[BUFFER_SIZE];
-
     int cipher_len = get_encrypted_secret(cipher_text);
-    cipher_text[cipher_len] = 0;
 
-    if (key_matches(2L, cipher_text, cipher_len, buffer)) {
-        printf("KEY MATCHES\n");
+    // init mpi
+    int size, rank;
+    MPI_Init(nullptr, nullptr);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    long work_per_node = MAX_KEY / size;
+    long lower = work_per_node * rank;
+    long upper = work_per_node * (rank + 1) - 1;
+    if (rank == size-1) {
+        upper = MAX_KEY;
     }
 
+    printf("Process %d: %ld - %ld \n", rank, lower, upper);
+
+    MPI_Request request;
+    MPI_Status status;
+    long key = 0L;
+    int found = 0;
+
+    MPI_Irecv(&key, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
+    for (long i = lower; i < upper; i++) {
+        MPI_Test(&request, &found, MPI_STATUS_IGNORE);
+        if (found) {
+            break;
+        }
+
+        if (key_matches(i, cipher_text, cipher_len, buffer)) {
+            key = i;
+            for (int node = 0; node < size; node++)
+                MPI_Send(&key, 1, MPI_LONG, node, 0, MPI_COMM_WORLD);
+            break;
+        }
+    }
+
+
+    if (rank == 0) {
+        MPI_Wait(&request, &status);
+        int len = decrypt(prepare_key_parity(key), cipher_text, buffer, cipher_len);
+        buffer[len] = 0;
+
+        printf("\nThe key %ld (found by %d) matched %s", key, status.MPI_SOURCE, buffer);
+    }
+
+    MPI_Finalize();
     cleanup_cipher();
 }
